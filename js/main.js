dadf5a7aa34ed757af7ae68c1b65dfd6744fddfa
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2020-06-09 00:25:51 +0800

    Add basic display to index.html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+window.onload = function main() {
+    let canvas = document.getElementById('render');
+    let gl = canvas.getContext('webgl');
+    if (!gl) {
+        return;
+    }
+}
